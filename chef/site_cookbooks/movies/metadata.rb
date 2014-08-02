041cf230f4c119e9f778056be1ecf60dec589567
David Exelby <david@sulaco.co.uk>
2014-08-02 14:32:44 +0100

    iptables -- open ports 80 & 443

---
+++ chef/site_cookbooks/movies/metadata.rb
@@ -0,0 +1,12 @@
+name             'movies'
+maintainer       'zz50'
+maintainer_email 'david@sulaco.co.uk'
+license          'All rights reserved'
+description      'Installs/Configures movies webapp'
+long_description 'Installs/Configures movies webapp'
+version          '0.1.0'
+
+depends "apache2"
+depends "mysql"
+depends "php"
+depends "simple-iptables"
--- chef/site_cookbooks/movies/recipes/default.rb
+++ chef/site_cookbooks/movies/recipes/default.rb
@@ -1,4 +1,5 @@
 include_recipe "apache2"
+include_recipe "simple-iptables"
 
 directory "/var/www/html" do
   recursive true
@@ -26,3 +27,18 @@ web_app "movies" do
   docroot "/var/www/movies/movies/html"
   directory_index "index.php"
 end
+
+#BEGIN CONFIGURE IPTABLES
+#Allow SSH
+simple_iptables_rule "ssh" do
+  rule "--proto tcp --dport 22"
+  jump "ACCEPT"
+end
+
+#Allow HTTP, HTTPS
+simple_iptables_rule "http" do
+  rule [ "--proto tcp --dport 80",
+         "--proto tcp --dport 443" ]
+  jump "ACCEPT"
+end
+#END CONFIGURE IPTABLES
